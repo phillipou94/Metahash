ba8af4c660c5697f6207c3e17461a86b8e5673db
Jiahe Kuang <jiahekuang@gmail.com>
2015-01-13 14:39:09 -0800

    links are optional now, and commments are not clickable of a link is not provided

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112184249) do
+ActiveRecord::Schema.define(version: 20150112200518) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150112184249) do
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "imageURL"
   end
 
   add_index "topics", ["user_id", "created_at"], name: "index_topics_on_user_id_and_created_at"
@@ -81,12 +82,11 @@ ActiveRecord::Schema.define(version: 20150112184249) do
     t.boolean  "liked"
     t.integer  "user_id"
     t.integer  "votable_id"
-    t.string   "votable_type"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "votes", ["user_id"], name: "index_votes_on_user_id"
-  add_index "votes", ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
+  add_index "votes", ["votable_id"], name: "index_votes_on_votable_id"
 
 end
